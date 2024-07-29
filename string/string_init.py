0e47697990d7d19a52f8fdadc60a2b6dc9bed52e
Asfi <20101115@uap-bd.edu>
2024-07-29 23:56:48 +0600

    string slicing

---
+++ string/string_init.py
@@ -0,0 +1,16 @@
+txt = "best things in life arefree"
+# print("free" in txt)  # true
+# print("expensive" not in txt)  # true
+# print("expensive" in txt)  # false
+
+# txt = "best things in life are free"
+# if "free" in txt:
+#     print("yes, free is in txt")
+
+# ---------Slicing---------
+print(txt[4:10])
+print(txt[4:])  # from 4 to end
+print(txt[:5])  # from start to five
+print(txt[:])  # whole thing
+print(txt[:-5])
+print(txt[-13:-4])
