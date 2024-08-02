dba11f4fed0d0b8caeb6776bd2b753c264206abd
Asfi <20101115@uap-bd.edu>
2024-08-02 21:38:10 +0600

    loop in a list

---
+++ Lists/loopinlist.py
@@ -0,0 +1,14 @@
+# thislist = ["apple", "banana", "cherry"]
+# for x in thislist:
+#     print(x)
+# print(len(thislist))
+thislist = ["apple", "banana", "cherry"]
+# for i in range(2):
+#     print(thislist[i])
+# i = 0
+# while i < len(thislist):
+#     print(thislist[i])
+#     i += 1
+
+# using list comprehension
+[print(x) for x in thislist]
