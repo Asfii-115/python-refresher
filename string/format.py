2288f2c9a16984848955749606ff8a91922d8592
Asfi <20101115@uap-bd.edu>
2024-09-30 13:06:57 +0600

    formatting strings

---
+++ string/format.py
@@ -0,0 +1,13 @@
+price = 50
+txt = f"it is very {'expensive' if price>50 else 'cheap'}"
+print(txt)
+
+fruit = "apples"
+txt2 = f"I love {fruit.upper()}"
+print(txt2)
+
+txt3 = "The price is {:.2f}"
+print(txt3.format(price))
+
+txt4 = "My order is {carname} and model {model}"
+print(txt4.format(carname="ford", model="mustang"))
