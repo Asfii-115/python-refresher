bcd56b2f3705199b4f9bda45cad007ff1d60f5b4
Asfi <20101115@uap-bd.edu>
2024-10-14 20:27:06 +0600

    dict access

---
+++ dict/access.py
@@ -0,0 +1,7 @@
+thisdict = {"brand": "Ford", "model": "Mustang", "year": 1964}
+print(thisdict.get("model"))
+print(thisdict.keys())
+
+thisdict["color"] = "red"
+print(thisdict)
+print(thisdict.items())
