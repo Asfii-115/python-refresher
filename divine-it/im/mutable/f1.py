7053e2005c3e06435b7703aaa295348fb928aed1
Asfi <20101115@uap-bd.edu>
2024-11-10 10:43:40 +0000

    mutables

---
+++ divine-it/im/mutable/f1.py
@@ -0,0 +1,24 @@
+x = 10
+print(id(x))
+x = x+1
+print(id(x))
+#mutable
+#list,dict,set etc
+list1 = [1,2,3]
+print(id(list1))
+list1.append(4)
+print(id(list1))
+
+def modify_list(lst):
+    lst.append(4)   # Modifies the original list
+
+def modify_string(s):
+    s = s + " world"  # Creates a new string, doesn't modify the original
+
+my_list = [1, 2, 3]
+modify_list(my_list)
+print(my_list)  # Output: [1, 2, 3, 4]
+
+my_string = "hello"
+modify_string(my_string)
+print(my_string)  # Output: "hello" (unchanged)
--- divine-it/lambda/f1.py
+++ divine-it/lambda/f1.py
@@ -1,7 +1,19 @@
-def cube(y):
-    return y*y*y
+# def cube(y):
+#     return y*y*y
 
  
-lambda_cube = lambda y: y*y*y
-print(cube(5))
-print(lambda_cube(5))
+# lambda_cube = lambda y: y*y*y
+# print(cube(5))
+# print(lambda_cube(5)) 
+
+
+r = [lambda arg=x: arg*10 for x in range(1,5)]
+for i in r:
+    print(i())
+is_even_list = [lambda arg=x: arg * 10 for x in range(1, 5)]
+for item in is_even_list:
+    print(item())
+
+
+max = lambda a,b: a if (a>b) else b
+print(max(1,2))    
