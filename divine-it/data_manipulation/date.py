e0f1faa1eaca58c5dcb902cbf3a2cbccf9963fd3
Asfi <20101115@uap-bd.edu>
2024-11-10 05:23:48 +0000

    datetime

---
+++ divine-it/data_manipulation/date.py
@@ -0,0 +1,29 @@
+# from datetime import datetime
+# from datetime import date
+
+# now = datetime.now()
+
+# print(now)
+# print(now.year)
+# print(now.month)
+# print(now.date)
+# print(now.day)
+# today = date.today()
+# print(today)
+
+# # Example 1  
+# s = now.strftime("%a %m %y")  
+# print('\nExample 1:', s)  
+  
+# # Example 2  
+# s = now.strftime("%A %-m %Y")  
+# print('\nExample 2:', s)  
+  
+# # Example 3  
+# s = now.strftime("%-I %p %S")  
+# print('\nExample 3:', s)  
+  
+# # Example 4  
+# s = now.strftime("%-j")  
+# print('\nExample 4:', s)
+
