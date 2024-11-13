f7bc2fa6f09f4d87bb0439d1284c753555368943
Asfi <20101115@uap-bd.edu>
2024-11-13 06:07:24 +0000

    dunder recall

---
+++ divine-it/date_manipulation/rc1.py
@@ -0,0 +1,13 @@
+from datetime import date, datetime
+
+today = date.today()
+print(today)
+
+now = datetime.now()
+print(now)
+
+import datetime
+print(dir(datetime))
+
+print(datetime.timedelta())
+
---
+++ divine-it/dunder/rc1.py
@@ -0,0 +1,10 @@
+# #------------dunder method------------
+
+# #def __str__(self)
+
+# provides string represtation of the class objects
+
+# objective: to return a human readable format
+
+# __repr__ for devs
+# __str__ for end users
