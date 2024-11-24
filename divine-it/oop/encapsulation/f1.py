6f1b83c4bdb384e141013c6ba71b5bb660fcba38
Asfi <20101115@uap-bd.edu>
2024-11-24 06:31:50 +0000

    encapsulation

--- divine-it/oop/encapsulation/f1.py
+++ divine-it/oop/encapsulation/f1.py
@@ -38,5 +38,52 @@ p3 = Person3('alice',33)
 p3.display()  
 #print(p3.__age)
 # Private attributes can still be accessed using name mangling (not recommended)
-print(p3._Person3__age)  # Output: 25     
+print(p3._Person3__age)  # Output: 25 
 
+
+#----getter & setter methods-----
+class Person4:
+    def __init__(self,name,age):
+        self.name = name
+        self.__age = age #private attribute
+
+    def get_age(self):
+        return self.__age 
+
+    def set_age(self, age):
+        if age>0:
+            self.__age = age
+        else:
+            print('age must be positive int')   
+
+p4 = Person4('jev',23)
+print(p4.get_age())
+p4.set_age(30)            
+print(p4.get_age())  
+
+
+#-----using property instead of getter & setter-------
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.__age = age  # Private attribute
+
+    @property
+    def age(self):
+        return self.__age
+
+    @age.setter
+    def age(self, value):
+        if value > 0:
+            self.__age = value
+        else:
+            print("Age must be positive")
+
+p = Person("Alice", 25)
+print(p.age)  # Output: 25 (Getter)
+
+p.age = 30    # Setter
+print(p.age)  # Output: 30
+
+p.age = -5    # Invalid input
+# Output: Age must be positive
